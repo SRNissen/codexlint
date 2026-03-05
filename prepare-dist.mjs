365df6b47e59b8e6ddaf2275dde2294d9b1fb2cb
SRNissen <4851097+SRNissen@users.noreply.github.com>
2026-03-05 11:30:43 +0100

    build stuff

---
+++ prepare-dist.mjs
@@ -0,0 +1,31 @@
+import { mkdir, rm, cp, copyFile } from "node:fs/promises";
+import path from "node:path";
+
+//Setup
+const rootDir = process.cwd();
+const distDir = path.join(rootDir, "dist");
+await rm(distDir, { recursive: true, force: true });
+await mkdir(path.join(distDir, "out", "src"), { recursive: true });
+
+//Source files
+await cp(
+    path.join(rootDir, "out", "src"),
+    path.join(distDir, "out", "src"),
+    {
+        recursive: true,
+        filter: (src, dst) => {
+            return !src.endsWith('.js.map');
+        }
+    }
+);
+
+//Extras
+async function addFile(filename) {
+    await copyFile(path.join(rootDir, filename), path.join(distDir, filename));
+}
+[
+    'CHANGELOG.md',
+    'LICENSE.TXT',
+    'README.md',
+    'package.json'
+].forEach(addFile);
